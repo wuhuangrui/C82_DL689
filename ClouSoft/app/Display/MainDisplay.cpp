a5aabb45e0255e5100eb23e498dc89ea4f797bcc
wuhuangrui <wuhuangrui@126.com>
2017-08-07 14:04:48 +0800

    1. add sleep time when get key.

--- ClouSoft/app/Display/MainDisplay.cpp
+++ ClouSoft/app/Display/MainDisplay.cpp
@@ -11354,10 +11354,11 @@ int TermCommpAndVersion(void *arg)
 				
 				ReadItemEx(BN2, PN0, 0x2107, bSoftInfo);
 				dwKeySec = GetClick();
-				DTRACE(DB_CRITICAL, ("TermCommpAndVersion GetKey().key is %d\n",GetKey()));//
+				DTRACE(DB_CRITICAL, ("TermCommpAndVersion GetKey().key is %d\n",GetKey().key));//
+				Sleep(200);
 				
-				//while (GetKey().key == KEY_OK)
-				while (GetKey() == KEY_OK)
+				while (GetKey().key == KEY_OK)
+				//while (GetKey() == KEY_OK)
 				{					
 					Sleep(200);
 					if (GetClick() - dwKeySec >= 2)	//确认键延时2s，就显示内部版本信息
