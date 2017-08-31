12ce88a7d2febbe7bc74b79a9244cfc3e7c01327
wuhuangrui <wuhuangrui@126.com>
2017-08-31 10:48:27 +0800

    1. correct VirtualComm file.

--- ClouSoft/arch/win/VirtualComm.h
+++ ClouSoft/arch/win/VirtualComm.h
@@ -11,17 +11,28 @@ public:
 	BYTE	 m_bByteSize;
 	BYTE	 m_bStopBits;
 	BYTE	 m_bParity;
+	BOOL	 m_OpenFlag;
+	
 
    CVirtualComm();
    ~CVirtualComm();
    BOOL Open(LPCTSTR szPort, DWORD dwBaudRate, BYTE bByteSize,
 		         BYTE bStopBits, BYTE bParity);
+   BOOL Open(int szPort, DWORD dwBaudRate, BYTE bByteSize,
+			         BYTE bStopBits, BYTE bParity);
    int Write(LPCVOID lpBuf, DWORD dwLength);
    int Read(LPVOID buf, DWORD dwLength);
+   BOOL  IsOpen();
+   int SetRecBuf(LPCVOID lpBuf, DWORD dwLength);
+
+   int SetAutoRecBuf(LPCVOID lpBuf, DWORD dwLength);
+   int ClearAutoRecBuf(void);
 
 private:
    BYTE m_sendBuf[1024];
    BYTE m_RecBuf[1024];
+   BYTE m_autoBuf[1024];
+   int  m_iAutoLen;
    void RunThread(void);
    CWinThread* m_pThread;
 
